c22eeda1031b1dc4729cc09616e54edad591ea1b
Sergei Andreev <s.andreev.it@gmail.com>
2020-04-18 14:33:45 +0500

    complete 3 steps

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import readlineSync from 'readline-sync';
 
-export const greeting = () => {
-    const actual = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${actual}`);
-}
+const greeting = () => {
+  const actual = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${actual}`);
+};
+
+export default greeting;
